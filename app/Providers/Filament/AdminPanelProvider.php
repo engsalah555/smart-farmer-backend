81ef7c7d7b14721f507c90ac1ad005c3234a20fe
Odai Bishr <odaibishr@gmail.com>
2026-04-29 01:57:54 +0300

    feat: change dashboard name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('زرعة')
             ->colors([
                 'primary' => '#10b981', // Emerald Green as requested
                 'gray' => Color::Zinc,
